eb89237bcac15fb7926dae15167f5f8e47d2b2c9
Akdan <akdan1111111@gmail.com>
2018-06-19 19:35:02 +0100

    chore set up dummy database for passengers and drivers

---
+++ server/dummydb/driverdb.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    id: 1,
+    username: 'danieladek',
+    email: 'maildaniel.me@gmail',
+    phone: '08182089681',
+    password: 'a.kan&_)3',
+    driverLicense: 'L/No Ak11129872',
+    carModel: 'AKD 321 986',
+    profilePic: 'danpic.jpg',
+    carImage: 'toyota.jpg',
+    gender: 'male'
+  }
+];
---
+++ server/dummydb/passengersdb.js
@@ -0,0 +1,12 @@
+const passenger = [
+  {
+    id: 1,
+    username: 'danieladek',
+    email: 'maildaniel.me@gmail',
+    phone: '08182089681',
+    password: 'a.kan&_)3',
+    gender: 'male'
+  }
+];
+
+// export default passenger;
