c560f22b349b1c03f32942af12c69902638dc124
Akdan <akdan1111111@gmail.com>
2018-06-20 21:16:13 +0100

    feature: validate input field for create ride offer

---
+++ server/middleware/validation/inputFields.js
@@ -0,0 +1,31 @@
+import { check } from 'express-validator/check';
+
+export default {
+  checker: [
+    check('departure')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please fill the departures field'),
+    check('arrival')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please fill the arrival field'),
+    check('spotInCar')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please specify spot in car field'),
+    check('data')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please specify date for the trip'),
+    check('time')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Please specify time for the trip')
+  ]
+};
