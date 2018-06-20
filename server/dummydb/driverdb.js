da12df693f4b0e4f6bc6b4edab5dd492d87ae9be
Akdan <akdan1111111@gmail.com>
2018-06-20 13:30:27 +0100

    feature: create drivers dummy database with array data structure

--- server/dummydb/driverdb.js
+++ server/dummydb/driverdb.js
@@ -6,9 +6,43 @@ export default [
     phone: '08182089681',
     password: 'a.kan&_)3',
     driverLicense: 'L/No Ak11129872',
-    carModel: 'AKD 321 986',
+    carPlateNo: 'AKD 321 986',
     profilePic: 'danpic.jpg',
     carImage: 'toyota.jpg',
-    gender: 'male'
+    gender: 'male',
+    carModal: 'Toyota |2018',
+    rideOffer: {
+      departure: 'Unilag -- Akoka road',
+      arival: 'Andela --235 Ikorodu Road',
+      time: '10:49 AM',
+      date: '27/11/2018',
+      spotInCar: 3,
+      carModel: 'Toyota |2018',
+      carPlateNo: 'AKD 321 986',
+      cost: '$3.0'
+    }
+  },
+  {
+    id: 2,
+    username: 'miko-rose',
+    email: 'miko-rose.mr@gmail',
+    phone: '08182089681',
+    password: 'e=(!m#i%ko-r)os*e',
+    driverLicense: 'L/No Ak11129872',
+    carPlateNo: 'AKD 321 986',
+    profilePic: 'miko.jpg',
+    carImage: 'Lexus.jpg',
+    gender: 'female',
+    carModal: 'Lexus |2018',
+    rideOffer: {
+      departure: 'Unilag -- Akoka road',
+      arival: 'Andela --235 Ikorodu Road',
+      time: '10:50 AM',
+      date: '25/06/2018',
+      spotInCar: 3,
+      carModel: 'Lexus |2018',
+      carPlateNo: 'AKD 321 986',
+      cost: '$2.0'
+    }
   }
 ];
