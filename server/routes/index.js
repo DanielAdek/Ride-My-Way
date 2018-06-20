eaf850658956a460244dee08530d0943f58fbe87
Akdan <akdan1111111@gmail.com>
2018-06-20 15:46:48 +0100

    feature: add route to fetch single ride offer from a driver

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,6 @@ import passengers from '../controllers/passengersController';
 const router = express.Router();
 
 router.get('/rides', passengers.getAllRides);
+router.get('/rides/:rideId', passengers.getSingleRide);
 
 export default router;
