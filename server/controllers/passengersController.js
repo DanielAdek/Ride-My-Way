4fa41df4868902996fe15c7763ae290bcf2f230f
Akdan <akdan1111111@gmail.com>
2018-06-20 15:45:44 +0100

    feature: create API to fetch single ride offer from a driver

--- server/controllers/passengersController.js
+++ server/controllers/passengersController.js
@@ -31,4 +31,40 @@ export default class Passengers {
       });
     });
   }
+
+  /**
+   * getSingleRide();
+     * @description get one ride
+     * @param {*} req HTTP request object
+     * @param {*} res HTTP response object
+     * @returns {object} json
+     */
+  static getSingleRide(req, res) {
+    let ride;
+    const requestId = parseInt(req.params.rideId, 10);
+    db
+      .forEach((driver) => {
+        if (driver.id === requestId) {
+          ride = driver.rideOffer;
+        }
+      });
+    return new Promise((resolve, reject) => {
+      if (!ride) {
+        reject(new Error('Cannot find any ride from this driver'));
+      }
+      resolve(ride);
+    })
+      .then((driverRide) => {
+        res.status(200).json({
+          message: 'Here You Are!',
+          driverRide
+        });
+      })
+      .catch((err) => {
+        res.status(200).json({
+          error: 'Oops Sorry!,',
+          message: err.message
+        });
+      });
+  }
 }
