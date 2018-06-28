8d702ecfd72d4d49a4c4d31884238224227bb660
Akdan <akdan1111111@gmail.com>
2018-06-29 00:50:30 +0100

    feature: add signup route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
-import rides from '../controllers/ridesController';
-import auth from '../middleware/validation/auth';
-import checkInput from '../middleware/validation/inputFields';
-import existing from '../middleware/validExist/isExisting';
+import user from '../src/controllers/userController';
+// import auth from '../middleware/validation/auth';
+// import checkInput from '../middleware/validation/inputFields';
+// import existing from '../middleware/validExist/isExisting';
 
 const router = express.Router();
 
-router.get('/rides', rides.getAllRides);
-router.get('/rides/:rideId', rides.getSingleRide);
-router.post('/rides', existing.rideOffer, checkInput.validOfferRide, auth.validateInput, rides.createRideOffer);
-router.post('/rides/:rideId/request', existing.requestRide, checkInput.validRequestRide, auth.validateInput, rides.requestRide);
+// router.get('/rides', rides.getAllRides);
+// router.get('/rides/:rideId', rides.getSingleRide);
+// router.post('/rides', existing.rideOffer, checkInput.validOfferRide, auth.validateInput, rides.createRideOffer);
+// router.post('/rides/:rideId/request', existing.requestRide, checkInput.validRequestRide, auth.validateInput, rides.requestRide);
+
+router.post('/auth/signup', user.createUser);
 
 export default router;
---
+++ server/src/controllers/userController.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcrypt';
+import db from '../models/connect';
+
+/**
+ * @class Users
+ */
+export default class Users {
+  /**
+   * createRideOffer();
+     * @description Create ride offers
+     * @param {*} req HTTP request object
+     * @param {*} res HTTP response object
+     * @returns {object} json
+     */
+  static createUser(req, res) {
+    const password = bcrypt.hashSync(req.body.password, 10);
+    const {
+      fullName, username, email
+    } = req.body;
+    const query = 'INSERT INTO users (fullName, username, email, password) VALUES ($1, $2, $3, $4) returning *';
+    const params = [fullName, username, email, password];
+    db
+      .query(query, params)
+      .then(newUser => res.status(201).json({ message: newUser.rows }))
+      .catch(err => res.status(500).json({ message: err.message }));
+  }
+}
