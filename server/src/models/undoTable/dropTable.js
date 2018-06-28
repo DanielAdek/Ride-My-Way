73a8b40f8bc00ee4fd205189d5489e61505eef0e
Akdan <akdan1111111@gmail.com>
2018-06-28 20:59:16 +0100

    chore: setup drop table functionality

---
+++ server/src/models/undoTable/dropTable.js
@@ -0,0 +1,5 @@
+import schema from './index';
+
+schema.dropUserTable();
+schema.dropRideTable();
+schema.dropRequestTable();
---
+++ server/src/models/undoTable/index.js
@@ -0,0 +1,25 @@
+import db from '../connect';
+
+export default {
+
+  dropUserTable() {
+    return new Promise((resolve, rejects) => {
+      const userModel = `
+    DROP TABLE IF EXISTS users CASCADE;`;
+    return db.query(userModel);
+    })
+    
+  },
+
+  dropRideTable() {
+    const rideModel = `
+    DROP TABLE IF EXISTS rides CASCADE;`;
+    return db.query(rideModel);
+  },
+
+  dropRequestTable() {
+    const requestModal = `
+    DROP TABLE IF EXISTS request CASCADE;`;
+    return db.query(requestModal);
+  }
+};
