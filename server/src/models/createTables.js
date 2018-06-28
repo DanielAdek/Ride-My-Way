fb470213d2464bfdf2e49706be8738281fd2a9b9
Akdan <akdan1111111@gmail.com>
2018-06-29 00:51:08 +0100

    chore: modify database

--- server/src/models/createTables.js
+++ server/src/models/createTables.js
@@ -6,11 +6,12 @@ export default {
     const userModel = `
     CREATE TABLE users (
       id SERIAL primary key,
-      firstName text NOT NULL,
-      lastName text NOT NULL,
-      role text NOT NULL,
+      fullName text NOT NULL,
+      username text NOT NULL,
       email text NOT NULL,
-      password text NOT NULL
+      password text NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );
     `;
     return db.query(userModel);
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,7 @@
 import schema from './createTables';
 
-schema.createUserTable();
+schema.createUserTable()
+  .then(() => console.log('successfully migrated'))
+  .catch(err => console.log(err.message));
 schema.createRideTable();
 schema.createRequestTable();
