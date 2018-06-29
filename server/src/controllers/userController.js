ba639ac1029f4c7ed0cde5ba935f6a9f6d5a6b79
Akdan <akdan1111111@gmail.com>
2018-06-29 01:27:05 +0100

    feature: create API endpoint functionality for login route

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -24,4 +24,25 @@ export default class Users {
       .then(newUser => res.status(201).json({ message: newUser.rows }))
       .catch(err => res.status(500).json({ message: err.message }));
   }
+
+  /**
+     * @returns {object} loginUser
+     * @param {*} req
+     * @param {*} res
+     */
+  static loginUser(req, res) {
+    const { email, password } = req.body;
+    const query = 'SELECT * FROM users WHERE email = $1 LIMIT 1;';
+    const param = [email];
+    db
+      .query(query, param)
+      .then((user) => {
+        if (user.rows[0] && bcrypt.compareSync(password, user.rows[0].password)) {
+          return res.status(200).json({ message: 'user logged in' });
+        }
+        return res.status(400).json({ message: 'email/password incorrect' });
+      }).catch((err) => {
+        res.status(500).json({ message: `server error ${err.message} ` });
+      });
+  }
 }
