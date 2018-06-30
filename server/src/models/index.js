8fa0b29f005eb9ff182ebd29a82971553998a5c9
Akdan <akdan1111111@gmail.com>
2018-07-01 00:02:24 +0100

    modify: add forign key contraint to models

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,11 @@
 import schema from './createTables';
 
 schema.createUserTable()
-  .then(() => console.log('successfully migrated'))
+  .then(() => console.log('User table successfully migrated'))
   .catch(err => console.log(err.message));
-schema.createRideTable();
-schema.createRequestTable();
+schema.createRideTable()
+  .then(() => console.log('Ride table successfully migrated'))
+  .catch(err => console.log(err.message));
+schema.createRequestTable()
+  .then(() => console.log('Request table successfully migrated'))
+  .catch(err => console.log(err.message));
