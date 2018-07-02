abbcfe29ac78c8468add9cc72cd6270b0f109857
Akdan <akdan1111111@gmail.com>
2018-07-02 09:04:57 +0100

    add validation to create rides route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,22 +2,26 @@ import express from 'express';
 import user from '../src/controllers/userController';
 import rides from '../src/controllers/ridesController';
 import request from '../src/controllers/requestController';
-// import auth from '../middleware/validation/auth';
-// import checkInput from '../middleware/validation/inputFields';
+import auth from '../src/midlewares/auth';
+import checkInput from '../src/validations/rides';
 // import existing from '../middleware/validExist/isExisting';
 
 const router = express.Router();
 
-// router.get('/rides', rides.getAllRides);
-// router.get('/rides/:rideId', rides.getSingleRide);
-// router.post('/rides', existing.rideOffer, checkInput.validOfferRide, auth.validateInput, rides.createRideOffer);
-// router.post('/rides/:rideId/request', existing.requestRide, checkInput.validRequestRide, auth.validateInput, rides.requestRide);
+/* dummy db routes
+ router.get('/rides', rides.getAllRides);
+ router.get('/rides/:rideId', rides.getSingleRide);
+ router.post('/rides', existing.rideOffer, checkInput.validOfferRide,
+  auth.validateInput, rides.createRideOffer);
+ router.post('/rides/:rideId/request', existing.requestRide,
+ checkInput.validRequestRide, auth.validateInput, rides.requestRide);
+*/
 
 router.post('/auth/signup', user.createUser);
 router.post('/auth/login', user.loginUser);
 router.get('/rides', rides.getAllRides);
 router.get('/rides/:rideId', rides.getSingleRide);
-router.post('/users/rides', rides.createRideOffer);
+router.post('/users/rides', checkInput.validOfferRide, auth.validateInput, rides.createRideOffer);
 router.post('/rides/:rideId/request', request.requestRide);
 router.get('/users/rides/:rideId/requests', request.getRequests);
 router.put('/users/rides/:rideId/requests/:requestId', request.updateRequest);
