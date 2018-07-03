b4794ab20427a311dccc39b94ff34a447420da29
Akdan <akdan1111111@gmail.com>
2018-07-04 00:58:56 +0100

    chore: create functionality to verify token

--- server/src/midlewares/auth.js
+++ server/src/midlewares/auth.js
@@ -1,5 +1,9 @@
 import { validationResult } from 'express-validator/check';
+import jwt from 'jsonwebtoken';
+import { config } from 'dotenv';
 
+config();
+const secret = process.env.SECRET;
 /**
  * @class auth
  */
@@ -20,6 +24,27 @@ export default class Auth {
     if (!error.isEmpty()) {
       return res.status(422).json({ errors: message });
     }
+    next();
+  }
+
+  /**
+     * verifyUser()
+     * @desc verify a user
+     * @param {object} req express request object
+     * @param {object} res express request object
+     * @param {function} next
+     * @returns {json} json
+     */
+  static verifyUser(req, res, next) {
+    const token = req.headers['x-access-token'] || req.query.token || req.headers.authorization;
+    if (!token) {
+      res.status(403).json({ message: 'No token provided' });
+    }
+    const decoded = jwt.verify(token, secret);
+    if (!decoded) {
+      res.status(403).json({ message: 'Invalid token' });
+    }
+    req.decoded = decoded;
     next();
   }
 }
