1e588d1538787fd770b8a30dd0d93774969dab8d
Akdan <akdan1111111@gmail.com>
2018-07-06 04:07:58 +0100

    add new user to test set up file

--- server/src/test/setup.spec.js
+++ server/src/test/setup.spec.js
@@ -4,7 +4,13 @@ const baseUrl = '/api/v1';
 const token = {
   user: `${jwt.sign({
     userid: 1,
+    username: 'danieladek',
     email: 'maildaniel.me1@gmail.com'
+  }, process.env.SECRET, { expiresIn: '24h' })}`,
+  userPlus: `${jwt.sign({
+    userid: 2,
+    username: 'anotheruser',
+    email: 'anotheruser.me1@gmail.com'
   }, process.env.SECRET, { expiresIn: '24h' })}`
 };
 
