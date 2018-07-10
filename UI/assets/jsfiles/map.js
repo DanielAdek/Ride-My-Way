27c3bec9def7904406d03a43bb4ad650a79c6e1e
Akdan <akdan1111111@gmail.com>
2018-07-10 07:32:19 +0100

    fix: eslint flags

--- UI/assets/jsfiles/map.js
+++ UI/assets/jsfiles/map.js
@@ -2,24 +2,24 @@ const divElement = document.querySelector('#map');
 
 function initMap() {
 
-    // map center
-    const unilag = {
-        lat: 6.5193,
-        lng: 3.3993
-    }
+  // map center
+  const unilag = {
+    lat: 6.5193,
+    lng: 3.3993
+  };
 
-    // map options
-    const options = {
-        zoom: 6,
-        center: unilag
-    }
+  // map options
+  const options = {
+    zoom: 6,
+    center: unilag
+  };
 
-    // new map
-    const map = new google.maps.Map(divElement, options);
+  // new map
+  const map = new google.maps.Map(divElement, options);
 
-    // map marker
-    const marker = new google.maps.Marker({
-        position: unilag,
-        map:map
-    })
+  // map marker
+  const marker = new google.maps.Marker({
+    position: unilag,
+    map
+  });
 }
