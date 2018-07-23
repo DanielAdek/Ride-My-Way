aea86f4ae103d06dd2cd6e9242ebc7cd452857f2
Akdan <akdan1111111@gmail.com>
2018-07-23 16:37:13 +0100

    feat: [Finish #159170301] forgot/reset password

---
+++ client/src/assets/forgetpassword.js
@@ -0,0 +1,63 @@
+/* eslint-env browser */
+const baseUrl = 'http://ridemyway-danieladek.herokuapp.com/api/v1';
+const body = document.querySelector('body');
+const email = document.querySelector('#email');
+const sendTokenBtn = document.querySelector('.send-token-btn');
+const error = document.querySelector('.error');
+const sucess = document.querySelector('.success-sent');
+
+const user = {
+  forgetPassword(event) {
+    event.preventDefault();
+    if (window.navigator.onLine === false) {
+      window.alert('It Seems Your computer is in offline mode'); // eslint-disable-line no-alert
+      return false;
+    }
+    if (email.value.trim() === '') {
+      error.textContent = 'Please complete this form';
+      setTimeout(() => {
+        error.textContent = null;
+      }, 3000);
+      return false;
+    }
+    error.textContent = 'Loading.....';
+    body.style.cursor = 'progress';
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Content-type', 'application/json');
+    fetch(`${baseUrl}/user/forgot-password`, {
+      method: 'PUT',
+      headers: myHeaders,
+      body: JSON.stringify({
+        email: email.value.trim()
+      })
+    }).then(res => res.json())
+      .then((data) => {
+        body.style.cursor = 'default';
+        if (!data.success) {
+          error.textContent = data.errors || data.message;
+          setTimeout(() => {
+            error.textContent = null;
+          }, 5000);
+        } else {
+          error.textContent = 'Please Wait!....';
+          setTimeout(() => {
+            error.textContent = null;
+          }, 2000);
+          setTimeout(() => {
+            sucess.textContent = `${data.message} 
+            <p>We will redirect you to Gmail</p>`;
+          }, 2000);
+          setTimeout(() => {
+            sucess.textContent = null;
+          }, 3000);
+          setTimeout(() => {
+            window.location.replace('https://mail.google.com');
+          }, 3000);
+        }
+      });
+  }
+};
+
+sendTokenBtn.addEventListener('click', user.forgetPassword);
+
---
+++ client/src/assets/resetPassword.js
@@ -0,0 +1,73 @@
+/* eslint-env browser */
+const baseUrl = 'http://ridemyway-danieladek.herokuapp.com/api/v1';
+const body = document.querySelector('body');
+const token = document.querySelector('#password-token');
+const password = document.querySelector('#newPassword');
+const confirmPass = document.querySelector('#confirmPassword');
+const resetPasswordBtn = document.querySelector('.reset-password-btn');
+const error = document.querySelector('.error');
+const success = document.querySelector('.success-message');
+
+const user = {
+  resetPassword(event) {
+    event.preventDefault();
+    if (window.navigator.onLine === false) {
+      window.alert('It Seems Your computer is in offline mode'); // eslint-disable-line no-alert
+      return false;
+    }
+    if (password.value.trim() === ''
+        || confirmPass.value.trim() === '') {
+      error.textContent = 'Please complete this form';
+      setTimeout(() => {
+        error.textContent = null;
+      }, 3000);
+      return false;
+    }
+
+    if (password.value.trim() !== confirmPass.value.trim()) {
+      error.textContent = 'Your password and confirm password does not match';
+      setTimeout(() => {
+        error.textContent = null;
+      }, 3000);
+      return false;
+    }
+    error.textContent = 'Loading.....';
+    body.style.cursor = 'progress';
+    const myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+    myHeaders.append('Content-type', 'application/json');
+    fetch(`${baseUrl}/user/reset-password`, {
+      method: 'PUT',
+      headers: myHeaders,
+      body: JSON.stringify({
+        token: token.value.trim(),
+        password: password.value.trim()
+      })
+    }).then(res => res.json())
+      .then((newPasword) => {
+        body.style.cursor = 'default';
+        if (!newPasword.success) {
+          error.textContent = newPasword.errors || newPasword.message;
+          setTimeout(() => {
+            error.textContent = null;
+          }, 5000);
+        } else {
+          error.textContent = 'Please Wait!....';
+          setTimeout(() => {
+            error.textContent = null;
+          }, 2000);
+          setTimeout(() => {
+            success.textContent = `${newPasword.message}`;
+          }, 2000);
+          setTimeout(() => {
+            success.textContent = null;
+          }, 5000);
+          setTimeout(() => {
+            window.location.replace('login.html');
+          }, 3900);
+        }
+      });
+  }
+};
+
+resetPasswordBtn.addEventListener('click', user.resetPassword);
