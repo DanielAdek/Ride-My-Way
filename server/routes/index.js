5490756d10ee5b3b4efba56f24cf45b0b7633a24
Akdan <akdan1111111@gmail.com>
2018-07-23 02:55:35 +0100

    update: add forget password route

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,34 +7,15 @@ import checkInput from '../src/validations/rides';
 import validateInput from '../src/validations/users';
 import existing from '../src/midlewares/validation';
 import requestAction from '../src/midlewares/request';
+import singleRoute from '../src/midlewares/singleRoute';
 
 const { verifyUser } = auth;
+const { forgetPassword } = singleRoute;
 const { email, username, ride } = existing;
 const { userSignUpDetails, userLoginDetails } = validateInput;
 const { validateRequestAction, validateRequestMessage, requestExit } = requestAction;
 
-// import existing from '../middleware/validExist/isExisting';
-
 const router = express.Router();
-
-/* dummy db routes
- router.get('/rides', rides.getAllRides);
- router.get('/rides/:rideId', rides.getSingleRide);
- router.post('/rides', existing.rideOffer, checkInput.validOfferRide,
-  auth.validateInput, rides.createRideOffer);
- router.post('/rides/:rideId/request', existing.requestRide,
- checkInput.validRequestRide, auth.validateInput, rides.requestRide);
-*/
-
-router.post(
-  '/auth/signup', userSignUpDetails,
-  auth.validateInput, email, username, user.createUser
-);
-
-router.post(
-  '/auth/login', userLoginDetails,
-  auth.validateInput, user.loginUser
-);
 
 router.get(
   '/user/rides', verifyUser,
@@ -44,13 +25,23 @@ router.get(
   '/user/requests', verifyUser,
   request.getOneUserRequests
 );
+
 router.get('/rides', rides.getAllRides);
 
 router.get('/rides/:rideId', rides.getSingleRide);
 
+router.get('/users/rides/:rideId/requests', verifyUser, request.getRequests);
+
+// router.get('/search/rides', rides.searchRide);
+
 router.post(
-  '/users/rides', verifyUser, checkInput.validOfferRide,
-  auth.validateInput, ride, rides.createRideOffer
+  '/auth/login', userLoginDetails,
+  auth.validateInput, user.loginUser
+);
+
+router.post(
+  '/auth/signup', userSignUpDetails,
+  auth.validateInput, email, username, user.createUser
 );
 
 router.post(
@@ -58,7 +49,12 @@ router.post(
   validateRequestMessage, requestExit, request.requestRide
 );
 
-router.get('/users/rides/:rideId/requests', verifyUser, request.getRequests);
+router.post(
+  '/users/rides', verifyUser, checkInput.validOfferRide,
+  auth.validateInput, ride, rides.createRideOffer
+);
+
+router.put('/user/reset-password', forgetPassword, user.resetPassword);
 
 router.put(
   '/users/rides/:rideId/requests/:requestId',
