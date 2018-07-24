cf12a666fa1564de0c3cf2a76983292bec3d562c
Akdan <akdan1111111@gmail.com>
2018-07-24 23:03:41 +0100

    modify: server response

--- server/src/midlewares/request.js
+++ server/src/midlewares/request.js
@@ -18,6 +18,7 @@ export default class Verify {
       next();
     } else {
       return res.status(400).json({
+        error: true,
         status: 'failed',
         message: 'action can only be accept or reject'
       });
@@ -41,6 +42,7 @@ export default class Verify {
         next();
       } else {
         return res.status(400).json({
+          error: true,
           status: 'failed',
           message: 'message can only contain letters'
         });
