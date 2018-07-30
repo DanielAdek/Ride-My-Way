cd7730cec307dd2e5b4fa8d0b94127718cbe3771
Akdan <akdan1111111@gmail.com>
2018-07-30 22:35:12 +0100

    chore: serve static files

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/api/v1', route);
 
-app.get('/*', (req, res) => {
+app.get('*', (req, res) => {
   res.status(200).send('404 Page Not Found');
 });
 
